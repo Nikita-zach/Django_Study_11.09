6a22279722d4d0a04b7785ef6b6a631590de8f22
Nikita-zach <akhmetshynm@gmail.com>
2024-09-20 18:33:50 +0200

    add homework for django lesson 3

--- home/models.py
+++ home/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    photo = models.ImageField(upload_to='events/')
+    text = models.TextField()
+
+    is_visible = models.BooleanField(default=True)
+    sort = models.IntegerField(default=0)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class EventList(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    events = models.ForeignKey(Event, on_delete=models.CASCADE)
+    sort = models.IntegerField(default=0)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Stuff(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    profession = models.CharField(max_length=50, unique=True)
+    photo = models.ImageField(upload_to='stuffs/')
+    text = models.TextField()
+
+    sort = models.IntegerField(default=0)
+    is_visible = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class StuffList(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    stuffs = models.ForeignKey(Stuff, on_delete=models.CASCADE)
+    sort = models.IntegerField(default=0)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class Gallery(models.Model):
+    photo = models.ImageField(upload_to='galleries/')
+
+    sort = models.IntegerField(default=0)
+    is_visible = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.photo.name
+
+class ContactInfo(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    info = models.TextField()
+    icon = models.ImageField(upload_to='contacts/')
+
+    sort = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name

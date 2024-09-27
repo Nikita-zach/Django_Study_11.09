e324d22f0e70728367f187b606b6c1d9591b9382
Nikita-zach <akhmetshynm@gmail.com>
2024-09-27 20:24:22 +0200

    update DjangoProj by lesson 4

--- home/views.py
+++ home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Category
+
 
 # Create your views here.
 def home_index(request):
-    return render(request, "index.html")
+
+    categories = Category.objects.filter(is_visible=True).order_by('sort')
+    context = {
+        "categories": categories,
+    }
+    return render(request, "index.html", context)
